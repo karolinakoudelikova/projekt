6597c5a88fcee41bde3bac6b99e7e6671b5b1f34
Karolina <koudelikova.kar@gmail.com>
2022-02-24 12:41:45 +0100

    Založení nové složky

--- ProgWarCamp/Lekce 4/vypis_mest.php
+++ ProgWarCamp/Lekce 4/vypis_mest.php
@@ -1,3 +1,10 @@
+<link rel="stylesheet" href="styly.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+</html>
+
 <?php
 
 $json = file_get_contents('https://data.cesko.digital/obce/1/obce.json');
@@ -9,17 +16,27 @@ echo $_GET["kraj"];
 echo "<br>";
 
 $pocetMest = 0;
+echo ('<table border="2" style="margin-left: auto; margin-right: auto;">');
 
 foreach ($obj["municipalities"] as $mesto) {
-    if ($mesto["adresaUradu"]["kraj"] === $_GET["kraj"]) {
+
+    if ($mesto["adresaUradu"]["kraj"] === $_GET["kraj"])
+    {
 
         $pocetMest = $pocetMest+1;
+
+
+        echo ('<tr>');
+        echo ('<td>');
         echo $mesto["hezkyNazev"];
-        echo "-";
+        echo ("</td>");
+        echo ("<td>");
         echo $mesto["datovaSchrankaID"];
-        echo "<br>";
+        echo ("</td>");
+        echo ("</tr>");
     }
 }
+echo('</table>');
 if ($pocetMest === 0) {
     echo "Byl vybrán špatný kraj!";
 }
