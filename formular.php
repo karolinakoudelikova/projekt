f238769d6a89267f939420563dbdcc0ac8c8c1d6
Karolina <koudelikova.kar@gmail.com>
2022-02-28 14:15:34 +0100

    přeorganizování složek

--- formular.php
+++
@@ -1,7 +0,0 @@
-
-
-<form method="post" action="zpracuj.php">
-    Příjmení: <input name="prijmeni"> <br>
-    Jméno: <input name="jmeno">
-    <input type="Submit" name="odesli">
-</form>
--- include.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once("funkce.php");
-require_once("date.php");
-
-echo secti($spoluzaci);
